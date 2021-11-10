83153b479f528a7c99d2591e2cb694995a8272a3
RenanSiman <renanvinte2@hotmail.com>
2021-11-09 21:48:34 -0300

    comment reduce game soundtrack

--- jogo.js
+++ jogo.js
@@ -23,8 +23,8 @@ let animais = [];
 const successSound = new Audio('sounds/success.wav'),
   errorSound = new Audio('sounds/error.wav');
 
-  successSound.volume = 0.02;
-  errorSound.volume = 0.02;
+  //successSound.volume = 0.02;
+  //errorSound.volume = 0.02;
 
 function sorteiaAnimais(quantos) {
   let animaisTodosCopia = animaisTodos.slice(0),
